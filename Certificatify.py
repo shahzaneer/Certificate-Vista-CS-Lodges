bf968c744bd00d934cb74794d3a973d8130459fa
Shahzaneer Ahmed <shahzaneer.dev@gmail.com>
2022-07-15 05:53:17 +0500

    CS lodges certificate >> Completed 🎉

---
+++ Certificatify.py
@@ -0,0 +1,114 @@
+# Imports
+from PIL import Image,ImageDraw,ImageFont
+import matplotlib.pyplot as plt
+import  pandas as pd
+from termcolor import colored
+from tqdm import tqdm_gui
+import os 
+from datetime import datetime
+import shutil
+from pdf_mail import sendpdf
+
+
+df = pd.read_excel("event.xlsx")
+emails = df['Emails'].values.flatten()
+naams = df["Name"].values.flatten()
+df.index = naams
+
+# Usually a .csv file but here i've used .xlsx file
+# Make sure to have 'Name' column in it
+file = pd.read_excel('event.xlsx')
+
+# organization Name
+# orgName = input("Enter Organization Name: ")
+orgName = "CS Lodges"
+
+# orgAcronym = input("Enter Organization Acronym: ")
+orgAcronym = "CSL"
+
+# speaker = input("Enter the name of Speaker: ")
+speaker = "Saad Hassan"
+
+
+eventName = input("Enter Event Name: ")
+# leadName = input("Lead Name: ")
+leadName = "Shahzaneer Ahmed"
+
+currentDate = datetime.date(datetime.now())
+
+# where certificates will be generated
+fname = 'certificates/'
+if os.path.exists(fname):
+    shutil.rmtree(fname)
+os.mkdir(fname)
+
+# Getting 'Name' columns from 'file'
+for names in tqdm_gui(file['Name']):
+    image = Image.new('RGB',(1920,1080),(255,255,255))
+
+
+    draw = ImageDraw.Draw(image)
+    font_path = './Almondita.ttf'
+
+    # font of ORG
+    fontdev = ImageFont.truetype('arial.ttf', size=35)
+    # font of Certificate
+    fontcert = ImageFont.truetype('arialbd.ttf', size=55)
+    # font of participant name
+    fontname = ImageFont.truetype('arial.ttf', size=35)
+    # font of signature
+    signature = ImageFont.truetype(font_path, 150)
+
+    # colors for various writings
+    colordev = 'rgb(128, 128, 128)'
+    colorcert = 'rgb(89, 89, 89)'
+    colorname = 'rgb(77, 148, 255)'
+    colorNameLead = 'rgb(229, 57, 53)'
+
+
+    # org logo
+    org_logo = Image.open('logo.png')
+    # Resizing the ORG Logo
+    org_logo = org_logo.resize((500,100))
+
+    # Left Side style Image
+    backgroundImage = Image.open('CertificateBg.png')
+
+    # Putting Logo & Left style Image
+    image.paste(backgroundImage, (0,0))
+    image.paste(org_logo, (950,125))
+
+
+
+    participation_message = f"is hereby awarded this Certificate of Participation on successfully attending \n{eventName} Organized by {orgName} delivered by\n {speaker}."
+
+    # draw.text((1040,143),'CS Lodges',font=fontdev,fill=colordev)
+    draw.text((950,250),'Certificate of Participation',font=fontcert,fill=colorcert)
+    draw.text((950,350), eventName + ' Participant',font=ImageFont.truetype('arial.ttf', size=32), fill=colordev)
+    draw.text((950,450),names,font=fontcert,fill=colorname)
+    draw.text((950,550),participation_message,font=ImageFont.truetype('arial.ttf', size=25),fill='rgb(102, 102, 102)')
+    draw.text((960,650),leadName,font=signature,fill=colorNameLead)
+    draw.line((950,800, 1520,800), fill='rgb(128, 128, 128)', width=3)
+
+    draw.text((950,820),f'CS Lodges,Community Manager',font=ImageFont.truetype('./arialbd.ttf', size=22),fill=colorcert)
+    draw.text((950,920), 'Signing Authority', font=ImageFont.truetype('arial.ttf', size=22), fill='rgb(128,128,128)')
+    draw.text((950,960), orgAcronym + ' Manager', font=ImageFont.truetype('./arialbd.ttf', size=25), fill='rgb(128,128,128)')
+    draw.text((1640,1020),'#CSLodges #CSFoundation',font=ImageFont.truetype('arial_italic.ttf', size=18),fill='rgb(211, 47, 47)')
+    draw.text((1640,950),'Certificate ID:',font=ImageFont.truetype('arial.ttf', size=20),fill=colorcert)
+    draw.text((1640,980),f'Issue Date: {currentDate}',font=ImageFont.truetype('arial.ttf', size=20),fill=colorcert)
+    image.save('certificates/'+names+'.pdf')
+
+    # for sending the email
+    senderEmail = 'shahzaneer.dev@gmail.com'
+    senderPass = 'ENTER_Your_Password_Here'
+    subject = "Certificate of Participation CS-Lodges"
+    bodyEmail = "Greetings!\n Hope this Email Finds you in the best of Health and Spirits!\n This is Shahzaneer Ahmed , Community Lead CS Lodges, We Thank you for your Participation in Event Organized by CS Lodges. We believe that it was an awesome Learning experience and are eager to arrange such events in the future as well. Thank you for being with us\n Shahzaneer Ahmed\n CS-Lodges\n shahzaneer.dev@gmail.com"
+    filename = names
+    location = "E:\CSLodgesCertificate\certificates/"
+    searchedEmails = df.Emails[names] # The beauty of Pandas >> I Love pandas 🎈
+    send = sendpdf(senderEmail,searchedEmails,senderPass,subject,bodyEmail,filename,location)
+    send.email_send()
+
+    print(f"Email Send to %s at Email Address : %s"%(names,searchedEmails))
+    
+
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <a href="#">
   <div align="center">
-    <img src="certificates/Muhammad Hamza.pdf" width='700'/>
+    <img src="certificates/FarhanAshraf.png" width='700'/>
   </div>
 </a>
 
@@ -10,13 +10,13 @@
 [![Open Source Love svg1](https://badges.frapsoft.com/os/v1/open-source.svg?v=103)](#)
 [![GitHub Forks](https://img.shields.io/github/forks/saadhaxxan/Car_Game_Python_Pygame.svg?style=social&label=Fork&maxAge=2592000)](https://www.github.com/dscuol/dsc-certificate-generator/fork)
 [![GitHub Issues](https://img.shields.io/github/issues/saadhaxxan/Car_Game_Python_Pygame.svg?style=flat&label=Issues&maxAge=2592000)](https://www.github.com/dscuol/dsc-certificate-generator/issues)
-[![contributions welcome](https://img.shields.io/badge/contributions-welcome-brightgreen.svg?style=flat&label=Contributions&colorA=red&colorB=black	)](#)
+[![contributions welcome](https://img.shields.io/badge/contributions-welcome-brightgreen.svg?style=flat&label=Contributions&colorA=red&colorB=black)](#)
 
 ## Installation steps
 
 1.Clone the Repo and install the requirements
 
-``` cmd
+```cmd
 git clone https://github.com/shahzaneer/Certificate-Vista-CS-Lodges.git
 cd Certificate-Vista-CS-Lodges
 pip install -r requirements.txt
@@ -28,34 +28,35 @@ pip install -r requirements.txt
 python generate.py
 ```
 
-
 ## Authors
+
 Extended the code from my Mentors Saad Hassan (Former GDSC LEAD UOL) and Muhammad Hamza Shakeel (Former GDSC Lead CUI):
 
 #### Muhammad Hamza Shakeel
-[![LinkedIn Link](https://img.shields.io/badge/Connect-Hamza-blue.svg?logo=linkedin&longCache=true&style=social&label=Connect
-)](https://www.linkedin.com/in/mhmzdev/)
+
+[![LinkedIn Link](https://img.shields.io/badge/Connect-Hamza-blue.svg?logo=linkedin&longCache=true&style=social&label=Connect)](https://www.linkedin.com/in/mhmzdev/)
 
 #### Saad Hassan
-[![LinkedIn Link](https://img.shields.io/badge/Connect-saadhaxxan-blue.svg?logo=linkedin&longCache=true&style=social&label=Connect
-)](https://www.linkedin.com/in/saad-haxxan/)
+
+[![LinkedIn Link](https://img.shields.io/badge/Connect-saadhaxxan-blue.svg?logo=linkedin&longCache=true&style=social&label=Connect)](https://www.linkedin.com/in/saad-haxxan/)
 
 You can also follow my GitHub Profile to stay updated about my latest projects:
 
 #### Shahzaneer Ahmed
-[![LinkedIn Link](https://img.shields.io/badge/Connect-Shahzaneer-blue.svg?logo=linkedin&longCache=true&style=social&label=Connect
-)](https://www.linkedin.com/in/shahzaneer/)
+
+[![LinkedIn Link](https://img.shields.io/badge/Connect-Shahzaneer-blue.svg?logo=linkedin&longCache=true&style=social&label=Connect)](https://www.linkedin.com/in/shahzaneer/)
 
 [![GitHub Follow](https://img.shields.io/badge/Connect-Shahzaneer-blue.svg?logo=Github&longCache=true&style=social&label=Follow)](https://github.com/shahzaneer)
 
 If you liked the repo then kindly support it by giving it a star ⭐!
 
 ## Contributions Welcome
+
 [![forthebadge](https://forthebadge.com/images/badges/built-with-love.svg)](#)
 
 If you find any bug in the code or have any improvements in mind then feel free to generate a pull request.
+If you want to contribute to the design only then contribute to the `Generate.py` file for convieniece
 
 If you face any issue, you can create a new issue in the Issues Tab and I will be glad to help you out.
 
 Copyright (c) 2022 Shahzaneer Ahmed
-
